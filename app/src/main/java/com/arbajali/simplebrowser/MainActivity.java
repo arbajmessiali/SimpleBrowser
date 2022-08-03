94386cbe4e4535326b5e51e7fe10e6c9ed3df84a
Intaj Ali <intajcse@gmail.com>
2022-08-03 15:45:07 +0530

    fix layout with webview

--- app/src/main/java/com/arbajali/simplebrowser/MainActivity.java
+++ app/src/main/java/com/arbajali/simplebrowser/MainActivity.java
@@ -10,22 +10,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     String home_url = "https://cn.bing.com/?FORM=BEHPTB&ensearch=1";
-    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = new WebView(this);
-        webView.loadUrl(home_url);
-        webView.setWebViewClient(new WebViewClient() {
+        WebView browser = (WebView) findViewById(R.id.wvShow);
+        browser.loadUrl(home_url);
+        browser.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
         });
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setJavaScriptEnabled(true);
+        browser.getSettings().setUseWideViewPort(true);
+        browser.getSettings().setLoadWithOverviewMode(true);
+        browser.getSettings().setJavaScriptEnabled(true);
+
     }
 }
