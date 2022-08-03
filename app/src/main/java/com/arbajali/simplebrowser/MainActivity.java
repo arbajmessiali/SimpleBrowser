3c9b5260747c774521d830ea39509b88ef733ebc
Intaj Ali <intajcse@gmail.com>
2022-08-03 14:23:48 +0530

    create webView

--- app/src/main/java/com/arbajali/simplebrowser/MainActivity.java
+++ app/src/main/java/com/arbajali/simplebrowser/MainActivity.java
@@ -1,4 +1,22 @@
 package com.arbajali.simplebrowser;
 
-public class MainActivity {
+import android.support.*;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    String home_url = "https://cn.bing.com/?FORM=BEHPTB&ensearch=1";
+    private WebView mywebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView.loadUrl(home_url);
+        WebSettings webSettings= mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
 }
